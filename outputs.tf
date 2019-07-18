648d211863be527b66c6868a4a818d4372cb1111
kaxil <kaxilnaik@gmail.com>
2019-07-18 19:59:34 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "application_url" {
-  value = "https://app.${var.base_domain}/"
-}
-
 # https://github.com/hashicorp/terraform/issues/1178
 resource "null_resource" "dependency_setter" {}
 output "depended_on" {
