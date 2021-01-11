81e161f954c8e30bbe3801ce84e3f4d34d3ae518
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-01-11 15:48:27 -0800

    Add pre-commit. Fix complaints.

--- pipeline/run_terraform.sh
+++ pipeline/run_terraform.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -xe
 
 terraform -v
@@ -5,18 +6,18 @@ terraform -v
 # unique deployment ID to avoid collisions in CI
 # needs to be 32 characters or less and start with letter
 DEPLOYMENT_ID=ci$(echo "$DRONE_REPO_NAME$DRONE_BUILD_NUMBER" | md5sum | awk '{print substr($1,0,30)}')
-echo $DEPLOYMENT_ID
+echo "$DEPLOYMENT_ID"
 
-cp providers.tf.example examples/$EXAMPLE/providers.tf
-cp backend.tf.example examples/$EXAMPLE/backend.tf
-cd examples/$EXAMPLE
+cp providers.tf.example "examples/$EXAMPLE/providers.tf"
+cp backend.tf.example "examples/$EXAMPLE/backend.tf"
+cd "examples/$EXAMPLE"
 sed -i "s/REPLACE/$DEPLOYMENT_ID/g" backend.tf
 
 terraform init
 
-cat .terraform/modules/modules.json | python -m json.tool
+python3 -m json.tool .terraform/modules/modules.json
 
-if [ $DESTROY -eq 1 ]; then
+if [ "$DESTROY" -eq 1 ]; then
   terraform destroy --auto-approve -var "deployment_id=$DEPLOYMENT_ID" -refresh=false -lock=false
 else
   terraform apply --auto-approve -var "deployment_id=$DEPLOYMENT_ID" --target=local_file.kubeconfig
