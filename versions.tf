d0d35af99d390d83b5ddda0e06a4d0c6d7982a6a
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-01-12 15:03:29 -0800

    Pin some versions

--- versions.tf
+++ versions.tf
@@ -2,19 +2,24 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     acme = {
-      source = "vancluever/acme"
+      source  = "vancluever/acme"
+      version = "~> 2.0.0"
     }
     helm = {
-      source = "hashicorp/helm"
+      source  = "hashicorp/helm"
+      version = "~> 2.0.1"
     }
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
+      version = "~> 1.13.3"
     }
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
+      version = "~> 3.0.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
+      version = "~> 3.0.0"
     }
   }
 }
