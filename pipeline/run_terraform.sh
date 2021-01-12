5a91256ba45b86791f0401bba94d3519aaea04a2
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-01-11 17:29:22 -0800

    Import more stuff from terraform-aws-astronomer-aws

--- pipeline/run_terraform.sh
+++ pipeline/run_terraform.sh
@@ -1,25 +1,28 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -xe
 
-terraform -v
+TERRAFORM="${TERRAFORM:-terraform-0.13}"
 
-# unique deployment ID to avoid collisions in CI
-# needs to be 32 characters or less and start with letter
-DEPLOYMENT_ID=ci$(echo "$DRONE_REPO_NAME$DRONE_BUILD_NUMBER" | md5sum | awk '{print substr($1,0,30)}')
-echo "$DEPLOYMENT_ID"
+$TERRAFORM -v
 
-cp providers.tf.example "examples/$EXAMPLE/providers.tf"
-cp backend.tf.example "examples/$EXAMPLE/backend.tf"
-cd "examples/$EXAMPLE"
-sed -i "s/REPLACE/$DEPLOYMENT_ID/g" backend.tf
+cp providers.tf.example examples/"$EXAMPLE"/providers.tf
+cp backend.tf.example examples/"$EXAMPLE"/backend.tf
+cd examples/"$EXAMPLE"
 
-terraform init
-
-python3 -m json.tool .terraform/modules/modules.json
-
-if [ "$DESTROY" -eq 1 ]; then
-  terraform destroy --auto-approve -var "deployment_id=$DEPLOYMENT_ID" -refresh=false -lock=false
+if [ "${DESTROY:-0}" -eq 1 ]; then
+  DEPLOYMENT_ID=ci$(echo "${CIRCLE_PROJECT_REPONAME}${CIRCLE_BUILD_NUM}" | md5sum | awk '{print substr($1,0,30)}')
+  echo "$DEPLOYMENT_ID"
+  sed -i "s/REPLACE/$DEPLOYMENT_ID/g" backend.tf
+  $TERRAFORM init
+  $TERRAFORM destroy --auto-approve -var "deployment_id=$DEPLOYMENT_ID"
 else
-  terraform apply --auto-approve -var "deployment_id=$DEPLOYMENT_ID" --target=local_file.kubeconfig
-  terraform apply --auto-approve -var "deployment_id=$DEPLOYMENT_ID"
+  DEPLOYMENT_ID=ci$(echo "$CIRCLE_PROJECT_REPONAME$CIRCLE_BUILD_NUM" | md5sum | awk '{print substr($1,0,30)}')
+  echo "$DEPLOYMENT_ID"
+  sed -i "s/REPLACE/$DEPLOYMENT_ID/g" backend.tf
+  $TERRAFORM init
+  $TERRAFORM apply --auto-approve -var "deployment_id=$DEPLOYMENT_ID"
+  # check that kubernetes is up and running
+  export KUBECONFIG=./kubeconfig-$DEPLOYMENT_ID
+  kubectl get namespaces
+  kubectl get pods --all-namespaces
 fi
