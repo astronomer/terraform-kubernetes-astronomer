e260df335ee0863cdf80e6ca98c4cf05b45ad024
pgvishnuram <vishnu@astronomer.io>
2021-10-01 14:02:38 +0530

    tf13 fixes

--- versions.tf
+++ versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     helm = {
-      source = "hashicorp/helm"
+      source  = "hashicorp/helm"
+      version = "~> 0.10"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
