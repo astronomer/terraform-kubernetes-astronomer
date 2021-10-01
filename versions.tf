c8ed3d7f441d9aac8d6042829de9045504a2e109
Daniel Hoherd <danielh@astronomer.io>
2021-10-01 14:05:59 -0700

    Terraform 0.13 and staging fixes (#33)
    * Changes to support terraform 0.13
    
    * Improve pre-commit
    
    * Swap drone for CircleCI
    
    * Shell syntax improvements

--- versions.tf
+++ versions.tf
@@ -1,3 +1,18 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 0.10"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
 }
