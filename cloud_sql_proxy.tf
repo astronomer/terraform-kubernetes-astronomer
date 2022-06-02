e2e8be68cf3042afb4373092f53b1a7408d7d1e5
Mishal Shah <mishal.shah@astronomer.io>
2022-06-03 01:06:16 +0530

    Removing old resource.

--- cloud_sql_proxy.tf
+++ cloud_sql_proxy.tf
@@ -1,16 +1,16 @@
-data "helm_repository" "rimusz" {
-  depends_on = [module.tiller]
-  count      = var.enable_cloud_sql_proxy ? 1 : 0
-  name       = "rimusz"
-  url        = "https://charts.rimusz.net"
-}
+#data "helm_repository" "rimusz" {
+#  depends_on = [module.tiller]
+#  count      = var.enable_cloud_sql_proxy ? 1 : 0
+#  name       = "rimusz"
+#  url        = "https://charts.rimusz.net"
+#}
 
 resource "helm_release" "cloud_sql_proxy" {
   depends_on = [module.tiller, helm_release.istio]
   count      = var.enable_cloud_sql_proxy ? 1 : 0
   name       = "pg-sqlproxy"
   version    = var.cloud_sql_proxy_helm_chart_version
-  repository = data.helm_repository.rimusz.0.name
+  repository = "https://charts.rimusz.net"
   chart      = "gcloud-sqlproxy"
   namespace  = kubernetes_namespace.astronomer.metadata.0.name
   wait       = true
--- kubecost.tf
+++ kubecost.tf
@@ -8,19 +8,19 @@ resource "kubernetes_namespace" "kubecost" {
   }
 }
 
-data "helm_repository" "kubecost" {
-  depends_on = [module.tiller]
-  count      = var.enable_kubecost ? 1 : 0
-  name       = "kubecost"
-  url        = "https://kubecost.github.io/cost-analyzer/"
-}
+#data "helm_repository" "kubecost" {
+#  depends_on = [module.tiller]
+#  count      = var.enable_kubecost ? 1 : 0
+#  name       = "kubecost"
+#  url        = "https://kubecost.github.io/cost-analyzer/"
+#}
 
 resource "helm_release" "kubecost" {
   depends_on = [module.tiller, helm_release.istio]
   count      = var.enable_kubecost ? 1 : 0
   name       = "kubecost"
   version    = var.kubecost_helm_chart_version
-  repository = data.helm_repository.kubecost.0.name
+  repository = "https://kubecost.github.io/cost-analyzer/"
   chart      = "cost-analyzer"
   namespace  = kubernetes_namespace.kubecost.0.metadata.0.name
   wait       = true
