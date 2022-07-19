d3734cfdcc55d97ffda68ac3d160b4d511a1deb8
Mishal Shah <mishal.shah@astronomer.io>
2022-07-19 12:31:11 +0530

    Removing version declaration.

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     helm = {
-      source  = "hashicorp/helm"
+      source = "hashicorp/helm"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
