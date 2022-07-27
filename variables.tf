56fc2d8be2773aee2ac3f81ea00bd57199f18f41
Mishal Shah <100714644+mishah334@users.noreply.github.com>
2022-07-27 10:12:33 +0530

    Update variables.tf
    Co-authored-by: Daniel Hoherd <daniel.hoherd@gmail.com>

--- variables.tf
+++ variables.tf
@@ -29,7 +29,7 @@ variable "astronomer_chart_git_repository" {
 
 variable "astronomer_version" {
   description = "Version of Helm chart to use, do not include a 'v' at the front"
-  default     = "0.12.0-alpha.1"
+  default     = "0.29.2"
   type        = string
 }
 
